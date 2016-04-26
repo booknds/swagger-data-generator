9a163d66cc799743062632b2310f16623bd5bed4
Eric <ehcorreia@hkmconsultingllc.com>
2016-04-26 17:36:37 -0400

    Testing, figured out how to chain promises

--- index.js
+++ index.js
@@ -1,80 +1,58 @@
 #! /usr/bin/env node
 
-var SwaggerParser = require("swagger-parser"),
-    YAML = require('yamljs'),
-    jsf = require('json-schema-faker');
+var _             = require('lodash');
+    SwaggerParser = require("swagger-parser"),
+    jsf           = require('json-schema-faker');
 
-var yamlData = YAML.load('./PetStore.yaml'),
-    defs,
-    mockedDefinitions;
+var swaggerDoc,
+  parser = new SwaggerParser();
 
-// SwaggerParser.validate(yamlData)
-//   .then(function(api) {
-//     console.log("API name: %s, Version: %s", api.info.title, api.info.version);
-//   })
-//   .catch(function(err) {
-//     console.error(err);
-//   });
-
-// SwaggerParser.dereference(yamlData)
-//   .then(function (api){
-//     console.log('BOOM');
-//     console.log(api.definitons.veterinarian);
-//     console.log('Homie');
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
-
-console.log("activity", yamlData);
-
-var parser = new SwaggerParser();
-parser.validate(yamlData)
+parser.parse('./PetStore.yaml')
   .then(function (api) {
-     console.log("hi",api);
+    swaggerDoc = api;
+    console.log("parsing", swaggerDoc);
+    return SwaggerParser.dereference(swaggerDoc);
   })
-  .catch(function(err) {
-    console.log('guhghg', err);
+  .then(function (api) {
+    console.log("Yeah it works!", api);
   });
 
 
-YAML.load('PetStore.yaml', function(result)
-{
-  // console.log(result.definitions.veterinarian);
-  // console.log(jsf(result.definitions.veterinarian, result));
-});
+SwaggerParser.dereference('./PetStore.yaml')
+  .then(function (bundle) {
+    // console.log('Bundling');
+    // console.log(bundle.definitions.pet);
 
-SwaggerParser.parse(yamlData)
-  .then(function (api) {
-    defs = api.definitions;
-    console.log(defs.address);
+    var definitions = _.map(bundle.definitions, function(definition) { 
+      return Object.assign({}, definition);
+    });
+    definitions[0].require = ['hi', 'dummy'];
+    console.log(definitions);
+    console.log(bundle.definitions.pet);
 
-    var sample = jsf(defs.pet);
-    console.log(sample);
-});
+    console.log(_.has(bundle.definitions.pet.properties, 'format'));
+    // console.log(_.values(bundle.definitions));
+    console.log(_.find(_.values(bundle.definitions), 'format', 'date'));
+    // var modifiedDefinition = [];
+    // make sure all properties are required
+    definitions.map(function (definition, index) {
+      var defProps = bundle.definitions[definition].properties;
+      var defPropNames = Object.keys(defProps);
+      var propsWithDateFormat = defPropNames.filter(function(name) {
+        return defProps[name].hasOwnProperty('format') && defProps[name].format === 'date';
+      });
+      console.log("yeee haw", propsWithDateFormat);
+    });
 
+    // console.log(jsf(bundle.definitions.pet));
+  })
+  .then()
+  .catch(function (err) {
+    console.log(err);
+  });
 
-var schema = {
-    //  "type": "object",
-    //  "description": "activity info",
-     properties: {
-         "state": {
-             "type": "string",
-             "description": "current activity state",
-             "enum": ["wait", "process", "over"]
-         },
-         "deadline": {
-             "type": "number",
-             "description": "deadline of the activity"
-         },
-         "remainDays": {
-             "type": "integer",
-             "description": "days left."
-         }
-     },
-     "required": ["state", "deadline", "remainDays", "needDays", "seasonNum"]
-}
-
-console.log(jsf(schema));
-
-// console.log( DataMocker( schema ) );
+SwaggerParser.dereference('./Perscription.json')
+  .then( function(api) { 
+    // console.log("JSON");
+    // console.log(api);
+  });
