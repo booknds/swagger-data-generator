e6b2ece3953720166432ecc230667db74b18a775
Eric <ehcorreia@hkmconsultingllc.com>
2016-04-27 16:41:59 -0400

    created generators for Swagger Specific formats and started using eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "rules": {
+        "no-debugger":1,
+        "no-console": [2, { allow: ["log"] }],
+        "no-extra-parens": 1,
+        "new-cap": 0,
+        "no-var": 0,
+        "object-shorthand": 0
+    },
+    "env": {
+        "es6": false,
+        "browser": false
+    },
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 5,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": false
+        },
+    },
+    "globals": {
+        "Materialize": true,
+        "describe": true,
+        "it": true,
+        "expect": true,
+        "angular": true,
+        "require": true,
+        "before": true,
+        "beforeEach": true,
+        "after": true,
+        "afterEach": true
+    }
+};
--- index.js
+++ index.js
@@ -1,58 +1,37 @@
 #! /usr/bin/env node
 
-var _             = require('lodash');
-    SwaggerParser = require("swagger-parser"),
-    jsf           = require('json-schema-faker');
+var _             = require('lodash'),
+    SwaggerParser = require('swagger-parser'),
+    jsf           = require('./lib/jsfConfig.js'),
+    swaggerDoc;
 
-var swaggerDoc,
-  parser = new SwaggerParser();
+function makeAllPropertiesRequired(apiDefinitions) {
+  var apiProperties = apiDefinitions.map(function (definitionPair) {
+    return Object.keys(definitionPair.properties);
+  });
 
-parser.parse('./PetStore.yaml')
+  apiDefinitions.forEach(function (definition, index) {
+    definition.required = apiProperties[index];
+    return definition;
+  });
+}
+
+SwaggerParser.parse('./PetStore.yaml')
   .then(function (api) {
     swaggerDoc = api;
-    console.log("parsing", swaggerDoc);
+    // console.log("parsing", swaggerDoc);
+    makeAllPropertiesRequired(_.values(swaggerDoc.definitions));
     return SwaggerParser.dereference(swaggerDoc);
   })
   .then(function (api) {
-    console.log("Yeah it works!", api);
-  });
-
-
-SwaggerParser.dereference('./PetStore.yaml')
-  .then(function (bundle) {
-    // console.log('Bundling');
-    // console.log(bundle.definitions.pet);
-
-    var definitions = _.map(bundle.definitions, function(definition) { 
-      return Object.assign({}, definition);
+    // console.log(api.definitions.pet.properties);
+    _.forEach(api.definitions, function (definition) {
+      // console.log(definition);
+      try {
+        var temp = jsf(definition);
+        console.log("jsf", temp);
+      } catch (err) {
+        console.log(err);
+      }
     });
-    definitions[0].require = ['hi', 'dummy'];
-    console.log(definitions);
-    console.log(bundle.definitions.pet);
-
-    console.log(_.has(bundle.definitions.pet.properties, 'format'));
-    // console.log(_.values(bundle.definitions));
-    console.log(_.find(_.values(bundle.definitions), 'format', 'date'));
-    // var modifiedDefinition = [];
-    // make sure all properties are required
-    definitions.map(function (definition, index) {
-      var defProps = bundle.definitions[definition].properties;
-      var defPropNames = Object.keys(defProps);
-      var propsWithDateFormat = defPropNames.filter(function(name) {
-        return defProps[name].hasOwnProperty('format') && defProps[name].format === 'date';
-      });
-      console.log("yeee haw", propsWithDateFormat);
-    });
-
-    // console.log(jsf(bundle.definitions.pet));
-  })
-  .then()
-  .catch(function (err) {
-    console.log(err);
-  });
-
-SwaggerParser.dereference('./Perscription.json')
-  .then( function(api) { 
-    // console.log("JSON");
-    // console.log(api);
   });
